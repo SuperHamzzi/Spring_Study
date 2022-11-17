03041b7d4e42f0ccdd76e6d29bebbfc70f6e31ea
SuperHamzzi <gksrnrrl6gh@naver.com>
2022-11-17 09:18:16 +0900

    11-16 commit

--- board/src/main/java/com/example/board/controller/BoardController.java
+++ board/src/main/java/com/example/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //HTTP요청을 받아서 응답을 하는 컴포넌트. 스프링 부트가 자동으로 Bean으로 생성한다.
 @Controller
@@ -11,7 +12,17 @@ public class BoardController {
     //list를 리턴한다는 것은
     //classpath:/templates/list.html
     @GetMapping("/")
-    public String list(){
+    public String list() {
         return "list";
+
+    }
+
+    @GetMapping("/board")
+    public String board(
+            @RequestParam("id") int id
+    ) {
+        System.out.println("id = " + id);
+        // id에 해당하는 게시물 읽기
+        return "board";
     }
 }
--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -3,6 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy{
 
 
